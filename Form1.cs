766606d0c8cc46a0e258bf7a3bf7dc801b541cd6
MordeKyle <Kyle@MordeKyle.com>
2014-04-28 02:16:33 -0500

    Version: 1.0 Beginning Robin
    04/28/2014 @ 02:14
    •Creating Repository

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Program: Total Sales
+  Author: Kyle McBride A02609917
+  Date: 04/28/2014
+  Description: In the Chap07 folder of the Student Sample Programs, you will find a file named Sales.txt. 
+               Figure 7-44 shows the file’s contents displayed in Notepad. Create an application that reads 
+               this file’s contents into an array, displays the array’s contents in a ListBox control, and 
+               calculates and displays the total of the array’s values.
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***				*/
+
+namespace Total_Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
